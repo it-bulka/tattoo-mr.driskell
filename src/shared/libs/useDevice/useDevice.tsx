297639d0b484a98aff5b5f50f935afe93bf3a23e
IT.bulka <i.it.bulka@gmail.com>
2025-03-11 13:22:01 +0100

    refactor useDevice: add dynamic size to detect

--- src/shared/libs/useDevice/useDevice.tsx
+++ src/shared/libs/useDevice/useDevice.tsx
@@ -1,19 +1,19 @@
 import { useState, useEffect } from 'react'
 
-export const useDevice = () => {
+export const useDevice = (size: number = 768) => {
   const [isMobile, setIsMobile] = useState<boolean>(false)
 
   useEffect(() => {
     const handleResize = () => {
       const isCoarse = window.matchMedia('(pointer: coarse)').matches
-      const isSmallScreen = window.innerWidth <= 768
+      const isSmallScreen = window.innerWidth <= size
       setIsMobile(isCoarse && isSmallScreen)
     }
     handleResize()
     window.addEventListener('resize', handleResize)
 
     return () => window.removeEventListener('resize', handleResize)
-  }, [])
+  }, [size])
 
   return isMobile
 }
