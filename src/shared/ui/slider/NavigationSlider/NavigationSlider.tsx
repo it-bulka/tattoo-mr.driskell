f050afa7c631ab150888da66804a1a5e83dcd5c3
IT.bulka <i.it.bulka@gmail.com>
2025-03-13 13:28:48 +0100

    add NavigationSlider

---
+++ src/shared/ui/slider/NavigationSlider/NavigationSlider.tsx
@@ -0,0 +1,75 @@
+import cls from './NavigationSlider.module.scss'
+import classNames from 'classnames'
+
+import { memo, useRef, PropsWithChildren } from 'react'
+import { SliderButton } from '@/shared/ui'
+import { Swiper } from 'swiper/react'
+import { Swiper as SwiperType } from 'swiper'
+import { Navigation } from 'swiper/modules'
+
+interface NavigationSliderProps {
+  className?: string
+  spaceBetween?: number,
+  slidesPerView?: number,
+  loop?: boolean,
+  speed?: number,
+}
+
+interface IBtns {
+  onPrevClick: () => void
+  onNextClick: () => void
+}
+
+const Btns = memo(({ onPrevClick, onNextClick }: IBtns) => {
+  return (
+    <div className={classNames("swiper-btns-container-2")}>
+      <SliderButton
+        className={"prev"}
+        onClick={onPrevClick}
+      />
+
+      <SliderButton
+        next
+        className="next"
+        onClick={onNextClick}
+      />
+    </div>
+  )
+})
+
+Btns.displayName = 'NavigationSliderBtns'
+
+export const NavigationSlider = memo(({
+  className,
+  children,
+  spaceBetween = 20,
+  slidesPerView = 4,
+  loop = false,
+  speed
+}: PropsWithChildren<NavigationSliderProps>) => {
+  const swiperRef = useRef<SwiperType>(null)
+
+  return (
+    <div className={classNames(cls.navigationSlider, 'container-slider-nav', {}, [className])}>
+      <Swiper
+        modules={[Navigation]}
+        slidesPerView={slidesPerView}
+        spaceBetween={spaceBetween}
+        loop={loop}
+        speed={speed}
+        onBeforeInit={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+      >
+        {children}
+      </Swiper>
+
+      <Btns
+        onPrevClick={()=> swiperRef.current?.slidePrev()}
+        onNextClick={()=> swiperRef.current?.slideNext()}
+      />
+    </div>
+  )
+})
+
+NavigationSlider.displayName = 'NavigationSlider'
