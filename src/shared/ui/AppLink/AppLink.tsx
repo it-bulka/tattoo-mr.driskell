3422e05c787d0cbbc288d6f8ed4ac2c2d4372b84
IT.bulka <i.it.bulka@gmail.com>
2025-03-18 14:26:26 +0100

    refactor appLink: add scrolling to anchor

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -2,20 +2,26 @@ import cls from './AppLink.module.scss'
 import classNames from 'classnames'
 import { NavLink, LinkProps } from 'react-router'
 import { PropsWithChildren, memo } from 'react';
+import { useToAnchorScroll } from '@/shared/libs';
 
 export interface AppLinkProps extends LinkProps{
   className?: string
+  anchorScroll?: boolean
 }
 export const AppLink = memo(({
   className,
   children,
+  anchorScroll = false,
+  to,
   ...rest
 }: PropsWithChildren<AppLinkProps>) => {
-
+  const anchorScrollHandle = useToAnchorScroll(to, anchorScroll)
   return (
     <NavLink
       className={classNames(cls.appLink, {}, [className])}
+      to={to}
       {...rest}
+      onClick={anchorScrollHandle}
     >
       {children}
     </NavLink>
