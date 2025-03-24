256520b13faca44db79bee824df1cb541f1df86e
IT.bulka <i.it.bulka@gmail.com>
2025-03-24 09:06:58 +0100

    refactor: - add position option for Modal - add possibility to pass modal close handler to children

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -3,20 +3,32 @@ import classNames from 'classnames'
 import { Portal } from '../Portal/Portal.tsx'
 import { Overlay } from '../Overlay/Overlay.tsx'
 import { useModal } from '@/shared/libs/useModal/useModal.tsx'
-import { PropsWithChildren } from 'react'
+import { ReactNode } from 'react'
 
+type ModalPosition = 'top left' | 'center'
 export interface ModalProps {
   className?: string
   isOpen?: boolean
   onClose?: () => void
+  position?: ModalPosition
+}
+
+type WithChildren = {
+  children: ReactNode | ((close: () => void) => ReactNode);
+}
+
+const positionToMap: Record<ModalPosition, string> = {
+  'center': cls.center,
+  'top left': classNames(cls.top, cls.left),
 }
 
 export const Modal = ({
   className,
   isOpen,
   onClose,
-  children
-}: PropsWithChildren<ModalProps>) => {
+  children,
+  position = 'center',
+}: ModalProps & WithChildren) => {
   const { isClosing, closeHandler } = useModal({
     onClose,
     isOpen
@@ -24,10 +36,14 @@ export const Modal = ({
 
   return (
     <Portal>
-      <div className={classNames(cls.modal, { [cls.opened]: isOpen, [cls.isClosing]: isClosing }, [className])}>
+      <div className={classNames(
+        cls.modal,
+        { [cls.opened]: isOpen, [cls.isClosing]: isClosing },
+        [className])}
+      >
         <Overlay onClick={closeHandler} />
-        <div className={cls.content}>
-          {children}
+        <div className={classNames(cls.content, {}, [positionToMap[position]])}>
+          {typeof children === 'function' ? children(closeHandler) : children}
         </div>
       </div>
     </Portal>
