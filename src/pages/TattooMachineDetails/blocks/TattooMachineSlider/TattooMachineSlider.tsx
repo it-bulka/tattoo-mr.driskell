4cc514d873fe2cc1fa2e0d5562ba237ed69de0da
IT.bulka <i.it.bulka@gmail.com>
2025-04-08 12:35:00 +0200

    fix Thumb swiper bug with no classList and swiper destroy

--- src/pages/TattooMachineDetails/blocks/TattooMachineSlider/TattooMachineSlider.tsx
+++ src/pages/TattooMachineDetails/blocks/TattooMachineSlider/TattooMachineSlider.tsx
@@ -1,18 +1,49 @@
 import cls from './TattooMachineSlider.module.scss'
 import classNames from 'classnames'
 import { Swiper as SwiperType } from 'swiper';
-import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { Thumbs, Pagination } from 'swiper/modules';
 import { useState, memo, useMemo } from 'react'
-import { type Slide, ImgSlide } from '@/shared/ui'
+import { ImgSlide } from '@/shared/ui'
 import ReactDOMServer from 'react-dom/server'
 import { PropsWithChildren } from 'react'
+import { TagType } from '@/shared/ui'
 
 interface TattoMachineSliderProps {
   className?: string
-  slides: Slide[]
+  slides: string[]
   isMobile?: boolean
 }
+
+interface ThumbsBtnsProps {
+  setThumbsSwiper: (swiper: SwiperType) => void
+  slides: string[]
+}
+let rerender = 0
+const ThumbsBtns = memo(({
+  setThumbsSwiper,
+  slides,
+}: ThumbsBtnsProps) => {
+  console.log('SHOW: rerender', rerender + 1)
+  return (
+    <Swiper
+      onSwiper={setThumbsSwiper}
+      spaceBetween={10}
+      slidesPerView={4}
+      freeMode={true}
+      watchSlidesProgress={true}
+      modules={[Thumbs]}
+      direction="vertical"
+      className="swiper-tattoo-machine"
+    >
+      {slides.map((slide) => (
+        <SwiperSlide key={slide}>
+          <img src={slide} alt="tatoo machine image" className={cls.img}/>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
+})
 
 const ThumbsSlider = ({
   slides,
@@ -20,37 +51,14 @@ const ThumbsSlider = ({
 }: PropsWithChildren<TattoMachineSliderProps>) => {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null)
 
-  const swiperProps = useMemo(() => {
-    const withThumbs: SwiperProps = {
-      thumbs: { swiper: thumbsSwiper },
-      modules: [Thumbs],
-      className: classNames(cls.slider, cls.withThumbs)
-    }
-
-    return withThumbs
-  }, [thumbsSwiper])
   return (
     <>
+      <ThumbsBtns slides={slides} setThumbsSwiper={setThumbsSwiper} />
+
       <Swiper
-        onSwiper={setThumbsSwiper}
-        spaceBetween={10}
-        slidesPerView={4}
-        freeMode={true}
-        watchSlidesProgress={true}
+        thumbs={{swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null}}
         modules={[Thumbs]}
-        direction="vertical"
-        className="swiper-tattoo-machine"
-
-      >
-        {slides.map((slide) => (
-          <SwiperSlide key={slide.id}>
-            <img src={slide.img} alt="tatoo machine image" className={cls.img}/>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      <Swiper
-        {...swiperProps}
+        className={classNames(cls.slider, cls.withThumbs)}
       >
         {children}
       </Swiper>
@@ -84,13 +92,15 @@ const PaginationSlider = ({
 export const TattooMachineSlider = memo(({
   className,
   slides,
-  isMobile = false
-}: TattoMachineSliderProps) => {
+  tags,
+  isMobile = false,
+  machineId
+}: TattoMachineSliderProps & { machineId: string, tags: TagType[]}) => {
 
   const slidesContent = useMemo(() => {
     return slides.map(slide => (
-      <SwiperSlide key={slide.id}>
-        <ImgSlide tags={slide.tags} img={slide.img} alt="tatoo machine image" className={cls.slide}/>
+      <SwiperSlide key={slide}>
+        <ImgSlide id={machineId} tags={tags} img={slide} alt="tatoo machine image" className={cls.slide}/>
       </SwiperSlide>
     ))
   }, [slides])
