6d4dfacca5dcb816983567919eb7e0edeb77232f
IT.bulka <i.it.bulka@gmail.com>
2025-04-16 16:11:18 +0200

    refactor cart middleware

--- src/entities/Cart/model/middleware/cartSyncMiddleware.tsx
+++ src/entities/Cart/model/middleware/cartSyncMiddleware.tsx
@@ -5,39 +5,51 @@ import { StateSchema } from '@/app/providers/StoreProvider/config/StateSchema.ts
 import { AppDispatch } from '@/app/providers/StoreProvider/config/store.ts'
 import { cartActions } from '@/entities/Cart/model/slice/cartSlice.tsx'
 
+let getState: (() => StateSchema) | null = null
+let dispatch: AppDispatch | null = null
+
+const debouncedSync = debounce(async () => {
+  if (!getState || !dispatch || !navigator.onLine) return
+
+  const state = getState()
+  const cart = state.cart
+
+  const items = Object.values(cart.entities).map(item => ({
+    product: item.productId,
+    amount: item.quantity,
+  }))
+
+  try {
+    const result = await dispatch(
+      cartApi.endpoints.syncCart.initiate({
+        userId: state.user.id,
+        orderItems: items,
+      })
+    ).unwrap()
+
+    dispatch(cartActions.setCartData(result?.data || []))
+    dispatch(cartActions.setBackSync(true))
+  } catch (error) {
+    console.error('Cart sync error:', error)
+  }
+}, 2000)
+
+
+const syncTriggerActions = [
+  'cart/addItem',
+  'cart/removeItem',
+  'cart/setItemAmount'
+]
+
 export const cartSyncMiddleware: Middleware = store => {
-  const debouncedSync = debounce(async () => {
-    const state = store.getState() as StateSchema
-    const cart = state.cart
-
-    if(!navigator.onLine) return
-
-    const items = Object.values(cart.entities).map(item => ({
-      product: item.productId,
-      amount: item.quantity,
-    }))
-
-    const dispatch = store.dispatch as AppDispatch
-    try {
-      const result = await dispatch(
-        cartApi.endpoints.syncCart.initiate({
-          userId: state.user.id,
-          orderItems: items,
-        })
-      ).unwrap();
-
-      dispatch(cartActions.setCartData(result?.data || []))
-      dispatch(cartActions.setBackSync(true))
-    } catch (error) {
-      console.error('Cart sync error:', error)
-    }
-  }, 2000)
+  getState = store.getState
+  dispatch = store.dispatch as AppDispatch
 
   return next => (action) => {
     const result = next(action)
 
     const typedAction = action as AnyAction
-    if (typedAction.type.startsWith('cart/') && !typedAction.type.startsWith('cart/fetchCart')) {
+    if (syncTriggerActions.includes(typedAction.type)) {
       debouncedSync()
     }
 
