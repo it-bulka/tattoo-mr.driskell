4201495b940932aaf9830849705ca77e6028166c
IT.bulka <i.it.bulka@gmail.com>
2026-05-30 20:12:20 +0200

    feat(shared): add localStorage constants

--- src/shared/consts/localStorages.tsx
+++ src/shared/consts/localStorages.tsx
@@ -1,2 +1,4 @@
 export const CART_LOCALSTORAGE = 'cart'
 export const IS_BACK_CART_SYNC_LOCALSTORAGE = 'isCartSync'
+export const ACCESS_TOKEN_LOCALSTORAGE = 'access_token'
+export const DEVICE_ID_LOCALSTORAGE = 'deviceId'
