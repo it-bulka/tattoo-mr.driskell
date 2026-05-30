48f2999cd3fe95064c64734a37231163e9097652
IT.bulka <i.it.bulka@gmail.com>
2026-05-30 20:12:55 +0200

    fix(auth): fix logout endpoint type and registration modal

--- src/shared/layouts/MainLayouts/MainLayout.tsx
+++ src/shared/layouts/MainLayouts/MainLayout.tsx
@@ -3,8 +3,12 @@ import { Outlet } from 'react-router'
 import { Header } from './Header/Header.tsx'
 import { Footer } from './Footer/Footer.tsx'
 import { ScrollUpToolbar } from '../../ui/ScrollUpToolbar/ScrollUpToolbar.tsx'
+import { RegistrationModal } from '@/features'
+import { useState } from 'react'
 
 export const MainLayout = () => {
+  const [isRegistrationOpen, setIsRegistrationOpen] = useState(false)
+
   return (
     <div className={cls.mainLayout}>
       <Header className={cls.noGrow}/>
@@ -13,6 +17,11 @@ export const MainLayout = () => {
       </main>
       <Footer className={cls.noGrow}/>
       <ScrollUpToolbar />
+
+      <RegistrationModal
+        isOpen={isRegistrationOpen}
+        onClose={() => setIsRegistrationOpen(false)}
+      />
     </div>
   )
 }
