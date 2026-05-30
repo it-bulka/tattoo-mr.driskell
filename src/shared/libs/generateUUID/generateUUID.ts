1899b18e05d1ba8cf232481fab0a47149ad3c34c
IT.bulka <i.it.bulka@gmail.com>
2026-05-30 20:11:35 +0200

    feat(shared): add generateUUID util and update shared exports

---
+++ src/shared/libs/generateUUID/generateUUID.ts
@@ -0,0 +1,13 @@
+export function generateUUID(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = Math.random() * 16 | 0
+    const v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
+
+export const setUUID = (key: string): string => {
+  const id = generateUUID()
+  localStorage.setItem(key, id)
+  return id
+}
