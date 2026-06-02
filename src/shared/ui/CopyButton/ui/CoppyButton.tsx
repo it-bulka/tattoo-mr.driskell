3c6ae7870db386dc6ccc2ae4d8e9d705a2d3c350
IT.bulka <i.it.bulka@gmail.com>
2026-06-02 17:01:33 +0200

    fix(ui): convert SVG kebab-case attrs to camelCase for JSX

--- src/shared/ui/CopyButton/ui/CoppyButton.tsx
+++ src/shared/ui/CopyButton/ui/CoppyButton.tsx
@@ -13,7 +13,7 @@ export const CopyButton = memo(({ textToCopy }: CopyButtonProps) => {
 
   return (
     <button onClick={() => handleCopy(textToCopy)} className={cls.btn}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <path d="M21.44 11.05L12.12 20.36a5 5 0 01-7.07-7.07l9.9-9.9a3 3 0 014.24 4.24l-9.9 9.9a1 1 0 01-1.41-1.41l9.19-9.2" />
       </svg>
 
--- src/shared/ui/Loaders/LoaderCircle/LoaderCircle.tsx
+++ src/shared/ui/Loaders/LoaderCircle/LoaderCircle.tsx
@@ -5,7 +5,7 @@ export const LoaderCircle = memo(() => {
   return (
     <div className={cls.loader}>
       <svg className={cls.circularLoader} viewBox="25 25 50 50">
-        <circle className={cls.loaderPath} cx="50" cy="50" r="20" fill="none" stroke-width="2"/>
+        <circle className={cls.loaderPath} cx="50" cy="50" r="20" fill="none" strokeWidth="2"/>
       </svg>
     </div>
   )
