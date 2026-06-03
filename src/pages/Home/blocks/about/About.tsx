631866b98de715f70f8bf420ad8a956605ac08fb
IT.bulka <i.it.bulka@gmail.com>
2026-06-03 11:16:53 +0200

    feat(home): add navigation to about page from about section button

--- src/pages/Home/blocks/about/About.tsx
+++ src/pages/Home/blocks/about/About.tsx
@@ -4,12 +4,15 @@ import { useTranslation } from 'react-i18next'
 import AboutImg from '@/shared/assets/others/about.png'
 import { Button } from '@/shared/ui'
 import { memo } from 'react'
+import { useNavigate } from 'react-router'
+import { getAboutPage } from '@/shared/config/routeConfig/routeConfig'
 
 interface AboutProps {
   className?: string
 }
 export const About = memo(({ className }: AboutProps) => {
   const { t } = useTranslation()
+  const navigate = useNavigate()
 
   return (
     <div className={classNames(cls.about, 'left-container', {}, [className])}>
@@ -18,7 +21,7 @@ export const About = memo(({ className }: AboutProps) => {
         <h3 className={cls.title}>{t('tattoo shop')}</h3>
         <p className={cls.content}>{t('about_info.1')}</p>
         <p className={cls.content}>{t('about_info.2')}</p>
-        <Button className={cls.btn}>{t('about company btn')}</Button>
+        <Button className={cls.btn} onClick={() => navigate(getAboutPage())}>{t('about company btn')}</Button>
       </div>
     </div>
   )
