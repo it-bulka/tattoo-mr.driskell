7b904db1fedf81c8e47dcc7d2b3c3401a50cb5cb
IT.bulka <i.it.bulka@gmail.com>
2026-06-03 11:51:11 +0200

    feat(home): navigate to /brands from brands block "see all" button

--- src/pages/Home/blocks/Brands/Brands.tsx
+++ src/pages/Home/blocks/Brands/Brands.tsx
@@ -1,17 +1,21 @@
 import cls from './Brands.module.scss'
 import classNames from 'classnames'
 import { useTranslation } from 'react-i18next'
-import { AppLink } from '@/shared/ui'
+import { useNavigate } from 'react-router'
+import { getBrandsPage } from '@/shared/config/routeConfig/routeConfig.tsx'
 import { BrandsSlider } from './BrandsSlider.tsx'
 
 export const Brands = () => {
   const { t } = useTranslation()
-  
+  const navigate = useNavigate()
+
   return (
     <>
       <div className={classNames(cls.header, 'container')}>
         <h3 className="pageTitle margin-0">{t('popular brands')}</h3>
-        <AppLink to="/" className={cls.link}>{t('see all')}</AppLink>
+        <button className={cls.link} onClick={() => navigate(getBrandsPage())}>
+          {t('see all')}
+        </button>
       </div>
 
       <BrandsSlider />
