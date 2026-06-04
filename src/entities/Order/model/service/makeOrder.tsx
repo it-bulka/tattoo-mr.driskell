7a839b7a9f1153f1de41c0d84c021c2621f5f6a9
IT.bulka <i.it.bulka@gmail.com>
2026-06-04 17:39:46 +0200

    fix(order): assert non-null userId in makeOrder data object

--- src/entities/Order/model/service/makeOrder.tsx
+++ src/entities/Order/model/service/makeOrder.tsx
@@ -63,7 +63,7 @@ export const makeOrder = createAsyncThunk<
         }
 
     const data = {
-      userId,
+      userId: userId!,
       paymentMethod: paymentType,
       deliveryMethod: delivery,
       buyer: { name, email, phone },
