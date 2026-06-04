72cc03804ccb7897f43dd0aeb30e8bc2e87618c4
IT.bulka <i.it.bulka@gmail.com>
2026-06-04 12:12:17 +0200

    feat(seo): wrap app in HelmetProvider and fix base index.html meta

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,20 +5,23 @@ import i18n from '@/shared/config/i18n/i18n.tsx';
 import { Suspense } from 'react'
 import { ErrorBoundary } from '@/shared/providers';
 import { StoreProvider } from './providers/StoreProvider'
+import { HelmetProvider } from 'react-helmet-async'
 import AppInit from './AppInit.tsx'
 
 export const App = () => {
   return (
-    <ErrorBoundary>
-      <StoreProvider>
-        <I18nextProvider i18n={i18n}>
-          <AppInit />
-          {/* TODO: add Page Loader */}
-          <Suspense fallback={<div>Loading ...</div>}>
-            <RouterProvider router={router} />
-          </Suspense>
-        </I18nextProvider>
-      </StoreProvider>
-    </ErrorBoundary>
+    <HelmetProvider>
+      <ErrorBoundary>
+        <StoreProvider>
+          <I18nextProvider i18n={i18n}>
+            <AppInit />
+            {/* TODO: add Page Loader */}
+            <Suspense fallback={<div>Loading ...</div>}>
+              <RouterProvider router={router} />
+            </Suspense>
+          </I18nextProvider>
+        </StoreProvider>
+      </ErrorBoundary>
+    </HelmetProvider>
   )
 }
