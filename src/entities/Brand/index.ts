50738d9d5639aee5557f5d7bf23655667dc3949d
IT.bulka <i.it.bulka@gmail.com>
2026-06-04 12:33:25 +0200

    refactor(selectors): replace selectBrandBySlug with memoized makeSelectBrandBySlug

--- src/entities/Brand/index.ts
+++ src/entities/Brand/index.ts
@@ -1,3 +1,3 @@
 export { useGetBrandsQuery } from './model/api/brandsApi.ts'
-export { selectBrandBySlug } from './model/selectors/brandSelectors.ts'
+export { makeSelectBrandBySlug } from './model/selectors/brandSelectors.ts'
 export type { Brand } from '@/shared/type/brand.ts'
--- src/entities/Brand/model/selectors/brandSelectors.ts
+++ src/entities/Brand/model/selectors/brandSelectors.ts
@@ -1,8 +1,10 @@
+import { createSelector } from '@reduxjs/toolkit'
 import { brandsApi } from '../api/brandsApi.ts'
 import { StateSchema } from '@/app/providers/StoreProvider/config/StateSchema.ts'
 import i18n from '@/shared/config/i18n/i18n.tsx'
 
-export const selectBrandBySlug = (slug: string) => (state: StateSchema) => {
-  const result = brandsApi.endpoints.getBrands.select(i18n.language)(state)
-  return result.data?.find(b => b.slug === slug)
-}
+export const makeSelectBrandBySlug = (slug: string) =>
+  createSelector(
+    (state: StateSchema) => brandsApi.endpoints.getBrands.select(i18n.language)(state),
+    (result) => result.data?.find(b => b.slug === slug)
+  )
