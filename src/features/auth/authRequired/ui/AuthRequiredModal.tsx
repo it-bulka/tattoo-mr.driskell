e07f0db13ee298a064dc67b83c932ca66a2d8c84
IT.bulka <i.it.bulka@gmail.com>
2026-06-05 10:20:11 +0200

    feat(auth): add AuthRequiredModal for unauthenticated like flow

---
+++ src/features/auth/authRequired/ui/AuthRequiredModal.tsx
@@ -0,0 +1,43 @@
+import { Modal } from '@/shared/ui'
+import { Auth } from '@/features/auth/components'
+import { useAuthModals } from '@/shared/libs/authModalsContext'
+import { useTranslation } from 'react-i18next'
+import cls from './AuthRequiredModal.module.scss'
+
+interface AuthRequiredModalProps {
+  isOpen: boolean
+  onClose: () => void
+}
+
+export const AuthRequiredModal = ({ isOpen, onClose }: AuthRequiredModalProps) => {
+  const { t } = useTranslation('favourites')
+  const { openLogin, openRegister } = useAuthModals()
+
+  const handleLogin = () => {
+    onClose()
+    openLogin()
+  }
+
+  const handleRegister = () => {
+    onClose()
+    openRegister()
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <Auth.Content>
+        <div className={cls.wrap}>
+          <Auth.Title>{t('auth required title')}</Auth.Title>
+          <div className={cls.actions}>
+            <button type="button" className={cls.link} onClick={handleLogin}>
+              {t('sign in')}
+            </button>
+            <button type="button" className={cls.link} onClick={handleRegister}>
+              {t('create account')}
+            </button>
+          </div>
+        </div>
+      </Auth.Content>
+    </Modal>
+  )
+}
