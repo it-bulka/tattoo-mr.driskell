cc1653b88981c7357ba73a911a0f7b7c5d862eb3
IT.bulka <i.it.bulka@gmail.com>
2026-06-05 10:27:16 +0200

    fix(home): simplify HomePageLoader skeleton layout

--- src/pages/Home/HomePageLoader.tsx
+++ src/pages/Home/HomePageLoader.tsx
@@ -4,24 +4,20 @@ import cls from '@/shared/ui/Skeleton/ui/pageLoader.module.scss'
 
 export const HomePageLoader = memo(() => {
   return (
-    <div>
-      <Skeleton height={500} variant="dark" />
-      <div className="container">
-        <div className={cls.loader}>
-          <Skeleton height={32} width={260} />
-          <div className={cls.grid4}>
-            {Array.from({ length: 4 }).map((_, i) => (
-              <div key={i} className={cls.productCard}>
-                <Skeleton height={220} border="4px" />
-                <Skeleton height={16} width="80%" />
-                <Skeleton height={20} width="40%" />
-              </div>
-            ))}
-          </div>
-          <Skeleton height={300} border="8px" />
+    <div className="container">
+      <div className={cls.loader}>
+        <Skeleton height={32} width={260} />
+        <div className={cls.grid4}>
+          {Array.from({ length: 4 }).map((_, i) => (
+            <div key={i} className={cls.productCard}>
+              <Skeleton height={220} border="4px" />
+              <Skeleton height={16} width="80%" />
+              <Skeleton height={20} width="40%" />
+            </div>
+          ))}
         </div>
+        <Skeleton height={300} border="8px" />
       </div>
-      <Skeleton height={200} variant="dark" />
     </div>
   )
 })
