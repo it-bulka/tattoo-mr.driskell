445407c5964ee3231ec8368aaec76e523a342f5b
IT.bulka <i.it.bulka@gmail.com>
2026-06-22 18:16:37 +0200

    fix(router): add ScrollRestoration to reset scroll position on navigation

--- src/app/config/router/RootRouter.tsx
+++ src/app/config/router/RootRouter.tsx
@@ -1,13 +1,18 @@
 import { MainLayout } from '@/shared/layouts'
+import { ScrollRestoration } from 'react-router'
 import { useInitCart } from '@/entities/Cart'
 import { useRestartInternet, useBeforeClosePage } from '@/shared/libs'
 import { useInitLikedProductsIds } from '@/features/addFavourite'
 
 export const RootRouter = () => {
-  // TODO: add just on auth here
   useInitCart()
   useRestartInternet()
   useBeforeClosePage()
   useInitLikedProductsIds()
-  return <MainLayout />
+  return (
+    <>
+      <ScrollRestoration />
+      <MainLayout />
+    </>
+  )
 }
