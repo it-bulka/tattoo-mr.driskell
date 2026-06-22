e14454013627246e000f90b0e1b718e3ccc568ef
IT.bulka <i.it.bulka@gmail.com>
2026-06-22 16:54:22 +0200

    fix(skeleton): align skeleton grids and card shape with redesigned components

--- src/shared/ui/Skeleton/ui/ProductCardSkeleton.tsx
+++ src/shared/ui/Skeleton/ui/ProductCardSkeleton.tsx
@@ -4,9 +4,11 @@ import cls from './pageLoader.module.scss'
 
 export const ProductCardSkeleton = memo(() => (
   <div className={cls.productCard}>
-    <Skeleton height={220} border="4px" />
-    <Skeleton height={16} width="80%" />
-    <Skeleton height={20} width="40%" />
+    <Skeleton height={240} />
+    <div className={cls.productCardContent}>
+      <Skeleton height={16} width="80%" border="6px" />
+      <Skeleton height={24} width="50%" border="6px" />
+    </div>
   </div>
 ))
 
