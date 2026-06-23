8be9eebb10a10714d96dd2b612f81a94fcb1500c
IT.bulka <i.it.bulka@gmail.com>
2026-06-23 12:40:27 +0200

    fix(favourites): wait for userId before fetching likes and favourite products

--- src/features/addFavourite/utils/useInitLikedProductsIds/useInitLikedProductsIds.tsx
+++ src/features/addFavourite/utils/useInitLikedProductsIds/useInitLikedProductsIds.tsx
@@ -1,11 +1,16 @@
-import { useEffect } from 'react'
-import { getFavouriteIds } from '../../model/service/getFavouriteIds.tsx'
-import { useAppDispatch } from '@/app/providers/StoreProvider/config/store.ts'
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getFavouriteIds } from "../../model/service/getFavouriteIds.tsx";
+import { useAppDispatch } from "@/app/providers/StoreProvider/config/store.ts";
+import { getUserId } from "@/entities";
 
 export const useInitLikedProductsIds = () => {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
+  const userId = useSelector(getUserId);
 
   useEffect(() => {
-    dispatch(getFavouriteIds())
-  }, [dispatch])
-}
+    if (userId) {
+      dispatch(getFavouriteIds());
+    }
+  }, [dispatch, userId]);
+};
--- src/pages/FavouritesPage/utils/useInitFavouritesFullData/useInitFavouritesFullData.tsx
+++ src/pages/FavouritesPage/utils/useInitFavouritesFullData/useInitFavouritesFullData.tsx
@@ -1,11 +1,16 @@
-import { useEffect } from 'react'
-import { useAppDispatch } from '@/app/providers/StoreProvider/config/store.ts'
-import { initFavouriteProducts } from '@/features/addFavourite/model/service/initFavouriteProducts'
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "@/app/providers/StoreProvider/config/store.ts";
+import { initFavouriteProducts } from "@/features/addFavourite/model/service/initFavouriteProducts";
+import { getUserId } from "@/entities";
 
 export const useInitFavouritesFullData = () => {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
+  const userId = useSelector(getUserId);
 
   useEffect(() => {
-    dispatch(initFavouriteProducts())
-  }, [dispatch])
-}
+    if (userId) {
+      dispatch(initFavouriteProducts());
+    }
+  }, [dispatch, userId]);
+};
