5287e08f17d05cbf6e56216fcd37e6730d8b3602
IT.bulka <i.it.bulka@gmail.com>
2026-06-23 11:49:14 +0200

    fix(auth): reset mutation state on modal close

--- src/features/auth/forgotPassword/ui/ForgotPasswordModal.tsx
+++ src/features/auth/forgotPassword/ui/ForgotPasswordModal.tsx
@@ -1,15 +1,15 @@
-import { Modal } from '@/shared/ui'
-import { Auth } from '@/features/auth/components'
-import { useSendForgotPasswordEmailMutation } from '@/features/auth/model/api/auth.tsx'
-import { getRtkApiMessage } from '@/shared/libs'
-import { ForgotPasswordContent } from './ForgotPasswordContent.tsx'
-import { ForgotPasswordSuccess } from './ForgotPasswordSuccess.tsx'
+import { Modal } from "@/shared/ui";
+import { Auth } from "@/features/auth/components";
+import { useSendForgotPasswordEmailMutation } from "@/features/auth/model/api/auth.tsx";
+import { getRtkApiMessage } from "@/shared/libs";
+import { ForgotPasswordContent } from "./ForgotPasswordContent.tsx";
+import { ForgotPasswordSuccess } from "./ForgotPasswordSuccess.tsx";
 
 interface ForgotPasswordModalProps {
-  isOpen: boolean
-  onClose: () => void
-  onOpenLogin: () => void
-  onOpenRegister: () => void
+  isOpen: boolean;
+  onClose: () => void;
+  onOpenLogin: () => void;
+  onOpenRegister: () => void;
 }
 
 export const ForgotPasswordModal = ({
@@ -18,28 +18,31 @@ export const ForgotPasswordModal = ({
   onOpenLogin,
   onOpenRegister,
 }: ForgotPasswordModalProps) => {
-  const [sendEmail, { isLoading, isSuccess, error, reset }] = useSendForgotPasswordEmailMutation()
+  const [sendEmail, { isLoading, isSuccess, error, reset }] =
+    useSendForgotPasswordEmailMutation();
 
   const handleClose = () => {
-    reset()
-    onClose()
-  }
+    reset();
+    onClose();
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={handleClose}>
-      <Auth.Content isLoading={isLoading} error={error ? getRtkApiMessage(error) : undefined}>
-        {!isSuccess
-          ? (
-            <ForgotPasswordContent
-              onSubmit={sendEmail}
-              onOpenLogin={onOpenLogin}
-              onOpenRegister={onOpenRegister}
-              isSubmitting={isLoading}
-            />
-          )
-          : <ForgotPasswordSuccess onOpenLogin={onOpenLogin} />
-        }
+      <Auth.Content
+        isLoading={isLoading}
+        error={error ? getRtkApiMessage(error) : undefined}
+      >
+        {!isSuccess ? (
+          <ForgotPasswordContent
+            onSubmit={sendEmail}
+            onOpenLogin={onOpenLogin}
+            onOpenRegister={onOpenRegister}
+            isSubmitting={isLoading}
+          />
+        ) : (
+          <ForgotPasswordSuccess onOpenLogin={onOpenLogin} />
+        )}
       </Auth.Content>
     </Modal>
-  )
-}
+  );
+};
--- src/features/auth/registration/ui/RegistrationModal.tsx
+++ src/features/auth/registration/ui/RegistrationModal.tsx
@@ -1,23 +1,28 @@
-import { Modal } from '@/shared/ui'
-import { Auth } from '@/features/auth/components'
-import { useRegisterMutation } from '../../model/api/auth.tsx'
-import { getRtkApiMessage } from '@/shared/libs'
-import { RegistrationContent } from './RegistrationContent.tsx'
-import { RegistrationSuccess } from './RegistrationSuccess.tsx'
+import { Modal } from "@/shared/ui";
+import { Auth } from "@/features/auth/components";
+import { useRegisterMutation } from "../../model/api/auth.tsx";
+import { getRtkApiMessage } from "@/shared/libs";
+import { RegistrationContent } from "./RegistrationContent.tsx";
+import { RegistrationSuccess } from "./RegistrationSuccess.tsx";
 
 interface RegistrationModalProps {
-  isOpen: boolean
-  onClose: () => void
-  onOpenLogin: () => void
+  isOpen: boolean;
+  onClose: () => void;
+  onOpenLogin: () => void;
 }
 
-export const RegistrationModal = ({ isOpen, onClose, onOpenLogin }: RegistrationModalProps) => {
-  const [registration, { isLoading, isSuccess, error, reset }] = useRegisterMutation()
+export const RegistrationModal = ({
+  isOpen,
+  onClose,
+  onOpenLogin,
+}: RegistrationModalProps) => {
+  const [registration, { isLoading, isSuccess, error, reset }] =
+    useRegisterMutation();
 
   const handleClose = () => {
-    reset()
-    onClose()
-  }
+    reset();
+    onClose();
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={handleClose}>
@@ -25,11 +30,15 @@ export const RegistrationModal = ({ isOpen, onClose, onOpenLogin }: Registration
         isLoading={isLoading}
         error={error && getRtkApiMessage(error)}
       >
-        {!isSuccess
-          ? <RegistrationContent onSubmit={registration} onOpenLogin={onOpenLogin} />
-          : <RegistrationSuccess />
-        }
+        {!isSuccess ? (
+          <RegistrationContent
+            onSubmit={registration}
+            onOpenLogin={onOpenLogin}
+          />
+        ) : (
+          <RegistrationSuccess />
+        )}
       </Auth.Content>
     </Modal>
-  )
-}
+  );
+};
